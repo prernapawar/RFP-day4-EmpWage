3c70ed522e3470e5c5fe558d4ff7a2092d628f04
prernapawar <100364823+prernapawar@users.noreply.github.com>
2022-04-07 17:59:42 +0530

    display welcome to emp wage meassage

---
+++ Start.java
@@ -0,0 +1,13 @@
+package EmpWage;
+/**
+ * @author Prerna
+ *
+ */
+public class Start {
+	public static void main (String []args) {
+	//print the message
+		System.out.println("Welcome to empolyee wage Computation");
+
+	}
+
+}
